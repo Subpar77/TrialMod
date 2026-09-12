b82a632939655705bab13e380b551492d56bbd12
Subpar77 <jjacobs6.jj@gmail.com>
2026-09-12 13:16:09 -0400

    Register block_item: foundry_brick Both block and block_item tested in client.

--- src/main/java/com/subpar77/trialmod/TrialMod.java
+++ src/main/java/com/subpar77/trialmod/TrialMod.java
@@ -1,6 +1,7 @@
 package com.subpar77.trialmod;
 
 import com.subpar77.trialmod.block.ModBlocks;
+import com.subpar77.trialmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -16,8 +17,6 @@ public class TrialMod {
     public static final String MODID = "trial_mod";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Items which will all be registered under the "trial_mod" namespace
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
 
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
@@ -25,7 +24,7 @@ public class TrialMod {
     public TrialMod(IEventBus modEventBus) {
 
         // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
+        ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
 
     }
---
+++ src/main/java/com/subpar77/trialmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.subpar77.trialmod.item;
+
+import com.subpar77.trialmod.TrialMod;
+import com.subpar77.trialmod.block.ModBlocks;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModItems {
+    private static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TrialMod.MODID);
+
+    public static final DeferredItem<BlockItem> FOUNDRY_BRICK_ITEM =
+            ITEMS.registerSimpleBlockItem(ModBlocks.FOUNDRY_BRICK, new Item.Properties());
+
+    public static void register(IEventBus modEventBus) {
+        ITEMS.register(modEventBus);
+    }
+}
